e566f5e6c201b892b1e3dc1a161f846e253473c8
Bohdan <bogdanbrohovetsky@gmail.com>
2022-07-17 19:17:33 +0300

    newfolders

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+
+console.log(1);
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+console.log(name);
+var name = 10;
+
+let big; 
+console.log(big);
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+// console.log(obj.name);
+console.log(obj["name"]);
+let arr = ['plum.png', 'orange.jpg', 'apple.mp3', 5, false];
+console.log(arr[0]);
+
+let time = [17,7,2022];
